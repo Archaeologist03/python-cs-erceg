9ef7e11f14f8ab0614814baa54fbfc7bc26cf46a
Archaeologist03 <omniiknight@hotmail.com>
2019-03-01 00:03:33 +0100

    Initializing repo for Ercegz intro to cs class, exams/labs.

---
+++ first_lib.py
@@ -0,0 +1,115 @@
+# Enter integer (0 - 155):
+# 99
+# Enter float:
+# 3.77
+# Enter character:
+# z
+# Enter string:
+# Howdy
+# 99 3.77 z Howdy
+
+userInt = int(input('Enter integer (0 - 155):\n'))
+print(userInt)
+userFloat = float(input('Enter float:\n'))
+print(userFloat)
+
+
+
+
+
+
+
+
+
+# ==================================== #
+
+
+
+
+
+
+
+
+# # Draw tree
+# print('   *')
+# print('  ***')
+# print(' *****')
+# print('*******')
+# print('  ***')
+
+
+# # Type your code here
+# print('/\\   /\\')
+# print('  o o')
+# print(' =   =')
+# print('  ---')
+
+
+
+
+
+
+
+
+# ==================================== #
+
+
+
+
+
+
+
+# userNum = int(input('Enter integer:\n'))
+
+# # Type your code here
+# squaredNum = userNum * userNum
+# cubedNum = userNum * userNum * userNum
+
+# print(userNum)
+# print('You entered: ' + str(userNum))
+# print(str(userNum) + ' squared is ' + str(squaredNum))
+# print('And ' + str(userNum) + ' ' 'cubed is ' + str(cubedNum) + ' !!')
+# userNum2 = int(input('Enter another integer:\n'))
+# print(userNum2)
+# print(str(userNum) + ' + ' + str(userNum2) + ' is ' + str(userNum + userNum2))
+# print(str(userNum) + ' * ' + str(userNum2) + ' is ' + str(userNum * userNum2))
+
+
+
+# # Type your code here
+# print('Hello World!')
+# print('How are you?')
+# print("   (I'm fine).")
+
+
+
+
+
+
+
+
+# ==================================== #
+
+
+
+
+
+
+# short_names = ['Gus', 'Bob', 'Zoe']
+
+# print(short_names[0])
+# print(short_names[1])
+# print(short_names[2])
+
+
+# male_names = { 'John', 'Bailey', 'Charlie', 'Chuck', 'Michael', 'Samuel', 'Jayden', 'Aiden', 'Henry', 'Lucas' }
+# female_names = { 'Elizabeth', 'Meghan', 'Kim', 'Khloe', 'Bailey', 'Jayden', 'Aiden', 'Britney', 'Veronica', 'Maria' }
+
+# # Use set methods to create sets all_names, neutral_names, and specific_names.
+
+# ''' Your solution goes here '''
+# all_names = male_names.union(female_names)
+# neutral_names = male_names.intersection(female_names)
+# specific_names = male_names.symmetric_difference(female_names)
+
+# print(specific_names)
