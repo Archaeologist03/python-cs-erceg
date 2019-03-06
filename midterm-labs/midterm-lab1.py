f4787418b879b37f85902a8b7328739989835755
Archaeologist03 <omniiknight@hotmail.com>
2019-03-06 11:49:55 +0100

    Adding exercices from midterm lab 1.

---
+++ midterm-labs/midterm-lab1.py
@@ -0,0 +1,58 @@
+# MIDTERM LAB 1
+
+
+# ======================================== #
+
+
+
+# # Type your code here
+# print('Information Technology')
+# print('Talladega College')
+# print('Talladega, AL 35160')
+
+
+# ======================================== #
+
+
+
+# userNum = int(input('Enter integer:\n'))
+
+# # Type your code here
+# squaredNum = userNum * userNum
+# cubedNum = userNum * userNum * userNum
+
+# print('You entered: ' + str(userNum))
+# print(str(userNum) + ' squared is ' + str(squaredNum))
+# print('And ' + str(userNum) + ' ' 'cubed is ' + str(cubedNum) + ' !!')
+
+# userNum2 = int(input('Enter another integer:\n'))
+# print(str(userNum) + ' + ' + str(userNum2) + ' is ' + str(userNum + userNum2))
+# print(str(userNum) + ' * ' + str(userNum2) + ' is ' + str(userNum * userNum2))
+
+# userNum3 = int(input('Enter a third integer:\n'))
+# print(str(userNum) + ' + ' + str(userNum2) + ' + ' + str(userNum3) + ' is ' + str(userNum + userNum2 + userNum3))
+# print(str(userNum) + ' * ' + str(userNum2) + ' * ' +  str(userNum3) + ' is ' + str(userNum * userNum2 * userNum3))
+
+
+
+
+
+# ======================================== #
+
+
+# # Draw tree
+# print('*********')
+# print('    *')
+# print('    *')
+# print('    *')
+# print('    *')
+# print('  *****')
+# print('*        *')
+# print('*')
+# print('*        *')
+# print('  *****')
+# # Type your code here
+
+
+
+# ======================================== #
